d11795bd103dace4d14ffbc41813f0191849c62d
ckswnskfk <ckswnskfk@hanmail.net>
2023-10-13 17:00:54 +0900

    feat: 메인 레이아웃

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,18 +2,31 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import SideBar from '@/components/SideBar';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Edu-Bridge',
-  description: '에듀브릿지 웹뷰',
+  description: '에듀브릿지',
 };
+
+const login = false;
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className='flex w-full h-full shadow-lg rounded-3xl'>
+          {login && (
+            <header className='bg-pink-50 w-1/12'>
+              <SideBar />
+            </header>
+          )}
+
+          <main className='w-full'>{children}</main>
+        </div>
+      </body>
     </html>
   );
 };
